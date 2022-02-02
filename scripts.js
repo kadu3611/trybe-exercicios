071c36d14eab3eb1482682b6ac28cc48e612c2ba
kadu3611 <kaduzeiro2013@gmail.com>
2022-02-02 16:51:15 -0300

    Arquivo javascript

---
+++ scripts.js
@@ -0,0 +1,11 @@
+const nota = 85;
+
+if(nota >= 80){
+    console.log("Parabéns, você foi aprovado!");
+}
+else if(nota >= 60){
+    console.log("Você esta na nossa lista de espera!");
+}
+else
+    console.log("Você foi reprovado");
+
