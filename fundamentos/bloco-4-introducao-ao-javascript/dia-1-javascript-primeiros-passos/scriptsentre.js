a5ed2c775999ccd8d95d0daae957f00abdd962dc
kadu3611 <kaduzeiro2013@gmail.com>
2022-02-02 18:03:23 -0300

    Arquivos criados do dia e exercicio

---
+++ fundamentos/bloco-4-introducao-ao-javascript/dia-1-javascript-primeiros-passos/scriptsentre.js
@@ -0,0 +1,9 @@
+let weeDay = 'Sabado';
+
+if(weeDay ==='segunda-feira' || weeDay ==='terça-feira' || weeDay ==='quarta-feira' || weeDay ==='quinta-feira' || weeDay ==='sexta-feira'){
+
+    console.log("Oba, mais um dia de aprendizado na Trybe >:D")
+}
+else{
+    console.log("FINALMENTE, descanso merecido UwU")
+}
---
+++ fundamentos/bloco-4-introducao-ao-javascript/dia-1-javascript-primeiros-passos/scriptsswits.js
@@ -0,0 +1,17 @@
+let candidato = "aprovado";
+
+switch (candidato){
+    case "aprovado":
+        console.log("Parabéns parça")
+        break;
+    case "reprovado":
+        console.log("Vai lá, estuda e tenta de novo!")
+        break;
+    case "lista de espera":
+        console.log("Você está lista de espera, continue estudando a resposta virá")
+        break;
+    default:
+        console.log("Parabéns parça")
+        break;
+
+}
