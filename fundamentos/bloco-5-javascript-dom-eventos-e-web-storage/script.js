6428a1882a5388a7eb80edca35f9e6ace52327d4
kadu3611 <kaduzeiro2013@gmail.com>
2022-02-09 19:43:35 -0300

    Esqueci dos outros commits, vai a penultima então

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/script.js
@@ -1,2 +1,24 @@
-let fundo = document.getElementsByTagName("body");
+
+let adm = document.getElementsByTagName("h1");
+adm[0].style.backgroundColor= 'green';
+
+
+
+let titulo = document.getElementsByTagName("h3");
+titulo[0].style.backgroundColor='blue';
+titulo[1].style.backgroundColor='blue';
+
+let emergencia = document.querySelectorAll(".emergency-tasks");
+emergencia[0].style.backgroundColor= 'pink';
+
+
+let dtitulo = document.querySelectorAll("h3");
+dtitulo[2].style.backgroundColor='black';
+dtitulo[3].style.backgroundColor='black';
+
+let noemergencia = document.querySelectorAll(".no-emergency-tasks");
+noemergencia[0].style.backgroundColor= 'yellow';
+
+let rodape = document.getElementById("footer-container");
+rodape.style.background = 'green'
 
