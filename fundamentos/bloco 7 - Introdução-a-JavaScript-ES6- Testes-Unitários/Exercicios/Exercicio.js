c42527559ae891236355155c4309a8af37991cbe
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-03-03 20:06:17 -0300

    Atividade do dia, falta parte II

--- fundamentos/bloco 7 - Introdução-a-JavaScript-ES6- Testes-Unitários/Exercicios/Exercicio.js
+++ fundamentos/bloco 7 - Introdução-a-JavaScript-ES6- Testes-Unitários/Exercicios/Exercicio.js
@@ -1,7 +1,45 @@
-if (true) {
-    // inicio do escopo do if
-    var userAge = '20';
-    console.log(userAge); // 20
-    // fim do escopo do if
+
+const testingScope = escopo => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
   }
-  console.log(userAge); // 20
+  /* console.log(`${ifScope} o que estou fazendo aqui ? :O`);  */// Se necessário esta linha pode ser removida.
+}
+
+testingScope(true);
+
+
+
+
+/* 
+function testingScope(escopo) {
+  if (escopo === true) {
+    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(true); */
+
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// Seu código aqui.
+
+
+const numeros = oddsAndEvens.sort((a, b) => a - b)
+
+console.log(numeros);
+
+
+ // será necessário alterar essa linha 😉
