c153887b5b9cfc57b3be1fbde8386026d2b94c46
Carlos Eduardo Fernandes Martins Salomao <kaduzeiro2013@gmail.com>
2022-03-09 15:57:35 -0300

    Atividade

---
+++ fundamentos/bloco-8-higher-order-functions-do-JavaScript-ES6/atividades.js
@@ -0,0 +1,6 @@
+const acordado = () => 'Acordando';
+const cafe = () => 'Bora tomar café!!';
+const dormir = () => 'Partiu dormir!!';
+
+const doingThings = (a) => a;
+console.log(doingThings(cafe()));
